635d2bd801253be5e562a4e3b4b937a49e5cd001
Makhmud <sapaevmahmud2002@gmail.com>
2025-05-08 14:14:40 +0500

    Update customer images in demo data for consistency

--- src/assets/data/demo.js
+++ src/assets/data/demo.js
@@ -2,28 +2,28 @@ export const customerInfo = [
     {id: 1, 
     name:'Viezh Robert', 
     comment:'“Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.',
-    img: '/public/John.png',
+    img: 'https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small_2x/Basic_Ui__28186_29.jpg',
     city: 'Warsaw, Poland',
     rating: '4.5'
     },
     {id: 2, 
     name:'Yessica Christy', 
     comment:'“I like it because I like to travel far and still can connect with high speed.”',
-    img: '/public/Yessica.png',
+    img: 'https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small_2x/Basic_Ui__28186_29.jpg',
     city: 'Shanxi, China',
     rating: '4.5'
     },
     {id: 3, 
     name:'Kim Young Jou', 
     comment:'“This is very unusual for my business that currently requires a virtual private network that has high security.”',
-    img: '/public/Kim.png',
+    img: 'https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small_2x/Basic_Ui__28186_29.jpg',
     city: 'Shanxi, China',
     rating: '4.5'
     },
     {id: 4, 
     name:'Kim Young Jou', 
-    comment:'“I like it because I like to travel far and still can connect with high speed.fdhshafidsaioashfishdfoahsfhsfashdifhasoihfashfodsahfioadshfoahsfiashfiodashfoashfadhfioadshfioadshfioasdhfiosadhfdishfsiahsfdihfoahsdofidhasofhdasoifhasdfihfhdsaofihaoihfoaisdhfoisahfdasiofhasdfadsiofhasdifhasoifhaoishfiaoshfoasidfhdsbvsv”',
-    img: '/public/Kim.png',
+    comment:'“I like it because I like to travel far and still can connect with high speed.”',
+    img: 'https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small_2x/Basic_Ui__28186_29.jpg',
     city: 'Shanxi, China',
     rating: '4.5'
     }
